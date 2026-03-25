6d1b2f8cb8918853c51a3ccf7c130acbd235f643
jacobhumston <humstonjacob@gmail.com>
2026-03-25 15:51:31 +0100

    fix attempt #2

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,34 @@
-import { Application, DefaultTheme, DefaultThemeRenderContext, JSX, PageEvent, Reflection, Renderer } from 'typedoc';
+import { appendFileSync, readFileSync } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import {
+    Application,
+    DefaultTheme,
+    DefaultThemeRenderContext,
+    JSX,
+    PageEvent,
+    Reflection,
+    Renderer,
+    RendererEvent
+} from 'typedoc';
 import { MyThemeContext } from './context.js';
-import { icons } from './icons.js';
+import { getThemeIcons } from './icons.js';
 
 class MyTheme extends DefaultTheme {
     constructor(renderer: Renderer) {
         super(renderer);
 
-        renderer.hooks.on('head.end', () => (
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-            />
-        ));
-
+        Object.assign(this.icons, getThemeIcons());
         renderer.hooks.on('head.end', () => <link rel="stylesheet" href="./style.css"></link>);
     }
 
     render(page: PageEvent): string {
         this.application.logger.info(`Rendering ${page.url}`);
-
-        // icons
-        this.icons.search = () => icons.search;
-        this.icons[64] = () => icons.function;
-
         return super.render(page);
+    }
+
+    async postRender(_event: RendererEvent): Promise<void> {
+        return super.postRender(_event);
     }
 
     getRenderContext(pageEvent: PageEvent<Reflection>): DefaultThemeRenderContext {
@@ -33,4 +38,12 @@ class MyTheme extends DefaultTheme {
 
 export function load(app: Application) {
     app.renderer.defineTheme('typedoc-neat-theme', MyTheme);
+
+    app.renderer.on(Renderer.EVENT_END, (event) => {
+        if (app.options.getValue('theme') === 'typedoc-neat-theme') {
+            const dir = dirname(dirname(fileURLToPath(import.meta.url)));
+            const style = readFileSync(join(dir, 'dist/style.css'), 'utf-8');
+            appendFileSync(join(event.outputDirectory, 'assets/style.css'), style);
+        }
+    });
 }
